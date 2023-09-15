7262fdf19163296d4cd374237f034368374be354
juarezjuniorgithub <juarez.barbosa@oracle.com>
2023-09-15 12:56:48 +0100

    @juarezjuniorgithub

---
+++ script/rsi-example.sql
@@ -0,0 +1,16 @@
+-- A High-Speed Data Ingestion Solution in Java Using MQTT, AMQP, and STOMP
+
+CREATE TABLE RETAILER (rank int, msr int, retailer varchar(255), name varchar (255), city varchar (255), 
+phone varchar (255), terminal_type varchar (255), weeks_active int, instant_sales_amt varchar(255), online_sales_amt varchar(255), 
+total_sales_amt varchar (255));
+COMMIT;
+
+--- DESCRIBE RETAILER;
+
+SELECT * FROM RETAILER;
+
+--- DELETE FROM RETAILER;
+--- COMMIT;
+
+--- DROP TABLE RETAILER;
+--- COMMIT;
